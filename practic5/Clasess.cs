8993769745a8c52ccf1b39d9721f056f753636fa
pablo <chayka.pavlik@mail.ru>
2022-11-01 20:22:35 +0700

    Добавьте файлы проекта.

---
+++ practic5/Clasess.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practic5
+{
+    enum Color//цвета
+    {
+        black = 0,
+        blue = 1,
+        green = 2,
+        red = 4,
+        white = 7
+    };
+    class Check
+    {
+        internal static int check(Color A)
+        { //проверка правильности цвета
+            switch (A)
+            {
+                case Color.black: return 1;
+                case Color.green: return 1;
+                case Color.blue: return 1;
+                case Color.red: return 1;
+                case Color.white: return 1;
+                default: return 0;
+            }
+        }
+    }
+    partial class ColorPrint
+    {
+        internal static void print_color(Color color)
+        {   //вывод цвета 
+            switch (color)
+            {
+                case Color.black: Console.WriteLine("BLACK"); break;
+                case Color.green: Console.WriteLine("GREEN"); break;
+                case Color.blue: Console.WriteLine("BLUE"); break;
+                case Color.red: Console.WriteLine("RED"); break;
+                case Color.white: Console.WriteLine("WHITE"); break;
+            }
+        }
+    }
+
+    partial class Circle
+    {
+        private int x, y;                      //центр круга
+        private int R;                         //радиус круга
+        private Color ColorFillCircle;               //цвет заливки
+        private Color ColorStrokeCircle;             //цвет обводки
+        public Circle(int XCoor, int YCoor, int Radius, Color Fill, Color Stroke)
+        {//конструкктор
+            x = XCoor; y = YCoor;
+
+
+            if (Radius >= 1) R = Radius;
+            else
+            {
+                Console.WriteLine("Error in Radius: " + R);
+                R = 1;
+            }
+
+
+            if (Check.check(Fill) == 1) ColorFillCircle = Fill;
+            else
+            {
+                Console.WriteLine("Error in color of fill: "); //если цвет неправильный, делаем цвет красным
+                ColorFillCircle = Color.red;
+            }
+            if (Check.check(Stroke) == 1) ColorStrokeCircle = Stroke;
+            else
+            {
+                Console.WriteLine("Error in color of fill");
+                ColorStrokeCircle = Color.red;
+            }
+
+        }
+        public partial double GetPerimetr();                           //получение периметра круга
+        public partial double GetSquare();                             //получение площади круга
+        public partial void MoveX(int dx);                              //перемещение по х координате
+        public partial void MoveY(int dy);                              //перемещение по y координате
+        public partial void print();                                   //вывод
+    }
+    partial class Triangle
+    {
+        int x1, y1, x2, y2, x3, y3;          //координаты вершин
+        Color ColorFillTriangle, ColorStrokeTriangle;           //цвета заливик и обводки
+        public Triangle(int X1Coor, int Y1Coor, int X2Coor, int Y2Coor, int X3Coor, int Y3Coor, Color Fill, Color Stroke)
+        { // конструктор
+
+            x1 = X1Coor; y1 = Y1Coor; x2 = X2Coor; y2 = Y2Coor; x3 = X3Coor; y3 = Y3Coor;
+
+
+            if (Check.check(Fill) == 1) ColorFillTriangle = Fill;
+            else
+            {
+                Console.WriteLine("Error in Color Fill"); //если цвет неправильный, делаем цвет красным
+                ColorPrint.print_color(Fill);
+                ColorFillTriangle = Color.red;
+            }
+            if (Check.check(Stroke) == 1) ColorStrokeTriangle = Stroke;
+            else
+            {
+                Console.WriteLine("Error in Color Stroke");
+                ColorPrint.print_color(Stroke);
+                ColorStrokeTriangle = Color.red;
+            }
+        }
+
+        private partial double GetLine(int x1, int y1, int x2, int y2);  //функция для определения стороны треугольник(ищет по т.Пифагора)
+        public partial double GetPerimetr();      //периметр треугольника
+        public partial double GetSquare();    //площадь треугольника
+        public partial void MoveX(int dx);   //пемерещение по х координате
+        public partial void MoveY(int dy);     //перемещение по y координате        
+        public partial void print();         //вывод
+    }
+
+    partial class VectorImage
+    {
+        List<Circle> circle = new List<Circle>();
+        List<Triangle> triangle = new List<Triangle>();
+
+        int Width;
+        int Height;
+
+
+        public VectorImage(int X, int Y, int r, Color fillcircle, Color strokecircle, int X1, int Y1, int X2, int Y2,
+        int X3, int Y3, Color filltriangle, Color stroketriangle, int width, int height)
+        {
+            Width = width;
+            Height = height;
+            circle.Add(new Circle(X, Y, r, fillcircle, strokecircle));
+            circle[0].X = X;
+            circle[0].Y = Y;
+            circle[0].Radius = r;
+            circle[0].Fill = fillcircle;
+            circle[0].Stroke = strokecircle;
+
+            triangle.Add(new Triangle(X1, Y1, X2, Y2, X3, Y3, filltriangle, stroketriangle));
+            triangle[0].X1 = X1;
+            triangle[0].Y1 = Y1;
+            triangle[0].X2 = X2;
+            triangle[0].Y2 = Y2;
+            triangle[0].X3 = X3;
+            triangle[0].Y3 = Y3;
+            triangle[0].FillT = filltriangle;
+            triangle[0].StrokeT = stroketriangle;
+        }//конструктор
+
+        public partial int GetQuantityCircle();  //нахождение количества кругов
+        public partial void AddCircle(int x, int y, int r, Color fill, Color stroke);    //добавление круга
+
+        public partial void AddTriangle(int X1, int Y1, int X2, int Y2, int X3, int Y3, Color filltriangle, Color stroketriangle); //добавления треугольника
+        public partial int GetQuantityTriangle(); // нахождение количества треугольников
+        public partial void DeleteCircle(int j);       //удаление круга по индексу
+        public partial void GetCircle(int i);          //получение круга по индексу
+
+        public partial void DeleteTriangle(int i);       //удаление треугольника по индексу
+        public partial void GetTriangle(int i);          //получение треугольтника по индексу
+
+        public partial void MoveX(int dx);           //перемещение всех фигур по х координате
+        public partial void MoveY(int dy);        //перемещение по y координате
+    }
+}
---
+++ practic5/Program.cs
@@ -0,0 +1,359 @@
+﻿using System.Drawing;
+
+namespace practic5
+{
+    partial class Circle
+    {
+        public int X
+        {
+            set { x = value; }
+        }
+        public int Y
+        {
+            set { y = value; }
+        }
+        public int Radius
+        {
+            set { R = (value >= 1) ? value : 1; }
+        }
+        public Color Fill
+        {
+            set
+            {
+                if (Check.check(value) == 1) ColorFillCircle = value;
+                else
+                {
+                    Console.WriteLine("Error in ColorFill");
+                    return;
+                }
+
+            }
+        }
+        public Color Stroke
+        {
+            set
+            {
+                if (Check.check(value) == 1) ColorStrokeCircle = value;
+                else
+                {
+                    Console.WriteLine("Error in ColorFill");
+                    return;
+                }
+            }
+        }
+
+        public partial void print() // вывод Круга
+        {
+            Console.WriteLine("x: " + x);
+            Console.WriteLine("y: " + y);
+            Console.WriteLine("R: " + R);
+            Console.WriteLine("ColorFill: ");   //вывод цвета заливик 
+            ColorPrint.print_color(ColorFillCircle);
+            Console.WriteLine("ColorStroke: ");  //вывод цвета обводки
+            ColorPrint.print_color(ColorStrokeCircle);
+        }
+
+        public partial double GetPerimetr()
+        {
+            return 2 * 3.14 * R;
+        }
+
+        public partial double GetSquare()
+        {
+            return 3.14 * R * R;
+        }
+
+        public partial void MoveX(int dx) // смещение по оси X
+        {
+            x += dx;
+        }
+
+        public partial void MoveY(int dy) // смещение по оси Y
+        {
+            y += dy;
+        }
+    }
+    partial class Triangle
+    {
+        public int X1
+        {
+            set { x1 = value; }
+        }
+        public int Y1
+        {
+            set { y1 = value; }
+        }
+        public int X2
+        {
+            set { x2 = value; }
+        }
+        public int Y2
+        {
+            set { y2 = value; }
+        }
+        public int X3
+        {
+            set { x3 = value; }
+        }
+        public int Y3
+        {
+            set { y3 = value; }
+        }
+        private partial double GetLine(int x1, int y1, int x2, int y2)  //функция для определения стороны треугольник(ищет по т.Пифагора)
+        {
+            int a = Math.Abs(x1 - x2);
+            int b = Math.Abs(y1 - y2);
+            double c = Math.Sqrt(a * a + b * b);
+            return c;
+
+        }
+        public Color FillT
+        {
+            set
+            {
+                if (Check.check(value) == 1) ColorFillTriangle = value;
+                else
+                {
+                    Console.WriteLine("Error in ColorFill");
+                    return;
+                }
+            }
+        }
+        public Color StrokeT
+        {
+            set
+            {
+                if (Check.check(value) == 1) ColorStrokeTriangle = value;
+                else
+                {
+                    Console.WriteLine("Error in ColorStroke");
+                    return;
+                }
+
+            }
+        }
+        public partial double GetPerimetr() //периметр треугольника
+        {
+            double a = GetLine(x1, y1, x2, y2);
+            double b = GetLine(x1, y1, x3, y3);
+            double c = GetLine(x2, y2, x3, y3);
+            return a + b + c;
+
+        }
+        public partial double GetSquare() //площадь треугольника
+
+        {
+            double a = GetLine(x1, y1, x2, y2);
+            double b = GetLine(x1, y1, x3, y3);
+            double c = GetLine(x2, y2, x3, y3);
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+        }
+        public partial void MoveX(int dx) //пемерещение по х координате
+
+        {
+            x1 += dx;
+            x2 += dx;
+            x3 += dx;
+        }
+        public partial void MoveY(int dy) //перемещение по y координате
+        {
+            y1 += dy;
+            y2 += dy;
+            y3 += dy;
+        }
+        public partial void print() //вывод
+        {
+            Console.WriteLine("x1 : " + x1);
+            Console.WriteLine("y1 : " + y1);
+
+            Console.WriteLine("x2 : " + x2);
+            Console.WriteLine("y2 : " + y2);
+
+            Console.WriteLine("x3 : " + x3);
+            Console.WriteLine("y3 : " + y3);
+
+            Console.WriteLine("ColorFill : ");
+            ColorPrint.print_color(ColorFillTriangle);
+
+
+            Console.WriteLine("ColorStroke : ");
+            ColorPrint.print_color(ColorStrokeTriangle);
+
+        }
+
+
+    }
+    partial class VectorImage
+    {
+        public partial int GetQuantityCircle() //нахождение количества кругов
+        {
+            return circle.Count;
+        }
+        public partial void AddCircle(int x, int y, int r, Color fill, Color stroke) //добавление круга
+        {
+            int k = circle.Count;
+            circle.Add(new Circle(x, y, r, fill, stroke));
+            circle[k].X = x;
+            circle[k].Y = y;
+            circle[k].Radius = r;
+            circle[k].Fill = fill;
+            circle[k].Stroke = stroke;
+
+        }
+        public partial void AddTriangle(int X1, int Y1, int X2, int Y2, int X3, int Y3, Color filltriangle, Color stroketriangle) //добавления треугольника
+        {
+            int k = triangle.Count;
+            triangle.Add(new Triangle(X1, Y1, X2, Y2, X3, Y3, filltriangle, stroketriangle));
+            triangle[k].X1 = X1;
+            triangle[k].Y1 = Y1;
+            triangle[k].X2 = X2;
+            triangle[k].Y2 = Y2;
+            triangle[k].X3 = X3;
+            triangle[k].Y3 = Y3;
+            triangle[k].FillT = filltriangle;
+            triangle[k].StrokeT = stroketriangle;
+
+        }
+        public partial int GetQuantityTriangle() // нахождение количества треугольников
+        {
+            return triangle.Count;
+        }
+        public partial void DeleteCircle(int j) //удаление круга по индексу
+        {
+            try
+            {
+                if (j < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                var add = circle;
+                circle.RemoveAt(j);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Индекса меньше нуля не существует");
+            }
+
+
+        }
+        public partial void GetCircle(int i) //получение круга по индексу
+        {
+            try
+            {
+                if (i >= circle.Count || i < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                Console.WriteLine($"Circle [{i}]:");
+                circle[i].print();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("По этому индексу нет треугольника!");
+            }
+
+        }
+
+        public partial void DeleteTriangle(int i) //удаление треугольника по индексу
+        {
+            try
+            {
+                if (i < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                for (int j = i; j < triangle.Count - 1; j++) triangle[j] = triangle[j + 1];
+                triangle.RemoveAt(0 + triangle.Count - 1);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Индекса меньше нуля не существует");
+            }
+
+        }
+
+        public partial void GetTriangle(int i) //получение треугольтника по индексу
+        {
+            try
+            {
+                if (i >= triangle.Count || i < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                Console.WriteLine($"Triangle [{i}]:");
+                triangle[i].print();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("По этому индексу нет треугольника!");
+            }
+
+
+        }
+        public partial void MoveX(int dx) //перемещение всех фигур по х координате
+        {
+            for (int i = 0; i < circle.Count; i++)
+            {
+                circle[i].MoveX(dx);
+            }
+
+            for (int i = 0; i < triangle.Count; i++)
+            {
+                triangle[i].MoveX(dx);
+            }
+
+        }
+        public partial void MoveY(int dy) //перемещение по y координате
+        {
+            for (int i = 0; i < circle.Count; i++)
+            {
+                circle[i].MoveY(dy);
+            }
+
+            for (int i = 0; i < triangle.Count; i++)
+            {
+                triangle[i].MoveY(dy);
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            VectorImage b = new VectorImage(12, 15, 18, Color.red, Color.blue, 12, 14, 15, 13, 19, 23, Color.white, Color.black, 29, 67); //конструктор
+            Console.WriteLine();
+            b.GetCircle(0);  //вывод круга для проверки
+            b.GetTriangle(0);  //вывод треугольника для проверки
+
+            Console.WriteLine();
+            b.AddCircle(15, 18, 21, Color.red, Color.blue);  //добавление нового круга
+            b.AddTriangle(18, 17, 1, 12, 8, -1, Color.white, Color.black);  //добавление нового треугольника
+            b.GetCircle(1);  //вывод нового круга для проверки
+            b.GetTriangle(1);  //вывод нового треугольника для проверки
+
+            Console.WriteLine();
+            b.AddCircle(19, 17, 11, Color.blue, Color.white); //добавление нового круга
+            b.GetCircle(2);  //вывод нового круга для проверки
+
+            Console.WriteLine();
+            b.MoveX(3);  //смещаем по х координате
+            b.MoveY(5);  //смещаем по y координате
+            Console.WriteLine("After MoveX(3) and MoveY(5):");
+            b.GetCircle(1);  //вывод круга для проверки MoveX
+            b.GetTriangle(1);  //вывод треугольника для проверки MoveY
+
+            Console.WriteLine();
+            b.DeleteCircle(1);  //удаляем круг
+            b.DeleteTriangle(1);  //удаляем треугольник
+            int count1 = b.GetQuantityCircle();  //выводим количество элементов в массиве Круга для проверки функций удаления
+            int count2 = b.GetQuantityTriangle();
+            Console.WriteLine("Size of circle after delete: " + count1);
+            Console.WriteLine("Size of triangle after delete: " + count2);
+
+        }
+
+    }
+}
